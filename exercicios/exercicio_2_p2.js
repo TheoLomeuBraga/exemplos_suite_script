ad905f4b995b864dd1c746fc344b633074e10bd9
TheoLomeuBraga <theolomeu@gmail.com>
2023-12-14 17:06:26 -0300

    a

--- exercicios/exercicio_2_p2.js
+++ exercicios/exercicio_2_p2.js
@@ -1,62 +1,57 @@
+//id busca:
+//customsearch3563
+
 /**
  * @NApiVersion 2.x
  * @NScriptType MapReduceScript
  */
-
-//id busca:
-//customsearch3563
-
-define(['N/log', 'N/search', 'N/record', 'N/runtime'], function(log, search, record, runtime) {
+define(['N/search', 'N/record'], function(search, record) {
 
     function getInputData() {
-        // Defina os parâmetros da busca salva que retorna os registros do instantâneo da cadeia de suprimentos
-        var searchId = 'sua_busca_salva_id'; // Substitua pelo ID real da sua busca salva
-        var filters = []; // Adicione quaisquer filtros necessários
+        // Defina os parâmetros da sua busca customizada aqui
+        var customSearch = search.load({
+            id: 'customsearch3563'
+        });
 
-        // Execute a busca
-        var resultSet = search.load({
-            id: searchId
-        }).run();
+        // Adquira os resultados da busca
+        var searchResults = customSearch.run().getRange({
+            start: 0,
+            end: 4 
+        });
 
-        // Retorne os resultados para serem processados pelo script
-        return resultSet;
+        return searchResults;
     }
 
     function map(context) {
-        // Processa cada resultado retornado pela busca
         var searchResult = JSON.parse(context.value);
 
-        // Obtém o ID do registro encontrado
-        var recordId = searchResult.id;
-
-        // Carrega o registro para atualização
         var myRecord = record.load({
-            type: 'TIPO_DE_REGISTRO', // Substitua pelo tipo de registro real
-            id: recordId
+            type: searchResult.recordType,
+            id: searchResult.id,
+            isDynamic: true
         });
 
-        // Atualiza o campo desejado
         myRecord.setValue({
-            fieldId: 'SEU_CAMPO', // Substitua pelo ID do campo a ser atualizado
-            value: 'NOVO_VALOR' // Substitua pelo novo valor do campo
+            fieldId: 'horizonte_passado',
+            value: 30
         });
 
-        // Salva as alterações
-        myRecord.save();
+        
+        myRecord.save()
+
+    }
+
+    function reduce(context) {
+
     }
 
     function summarize(summary) {
-        // Loga informações sobre o resumo do processo Map/Reduce
-        log.audit({
-            title: 'Resumo do Map/Reduce',
-            details: 'Total de registros processados: ' + summary.inputSummary.total
-        });
     }
 
     return {
         getInputData: getInputData,
         map: map,
+        reduce: reduce,
         summarize: summarize
     };
-
 });
